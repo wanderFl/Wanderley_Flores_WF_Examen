a6412fff7291250bbbf792bfb09dc619e7f5bfae
laboratorio <wanderley.flores@udla.edu.ec>
2023-10-25 08:16:24 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wanderley_Flores_WF_Examen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
